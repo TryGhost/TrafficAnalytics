56ce463a1078f514ed15f8f1fb499f2a1ce87f51
Joe Grigg <joe@ghost.org>
2025-04-14 14:33:50 +0100

    Added a temporary fully permissive CORS policy

--- src/app.js
+++ src/app.js
@@ -11,6 +11,20 @@ app.use((req, res, next) => {
     next();
 });
 
+// CORS middleware to allow requests from all origins
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    
+    next();
+});
+
 app.get('/', (req, res) => {
     res.status(200).send('Hello World - Github Actions Deployment Test');
 });
