e009d5116bbe1d42faf7c4949e3d1c44f86a9cdf
Chris Raible <chris@ghost.org>
2025-04-14 13:46:12 -0700

    Added eslint rules for newlines and trailing spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     plugins: ['ghost'],
     extends: [
         'plugin:ghost/node'
-    ]
+    ],
+    rules: {
+        'eol-last': ['error', 'always'],
+        'no-trailing-spaces': 'error'
+    }
 };
--- src/utils/query-params.js
+++ src/utils/query-params.js
@@ -6,11 +6,11 @@
 function filterQueryParams(url) {
     // Get query parameters without creating a full URL object
     const searchParams = new URLSearchParams(url.split('?')[1] || '');
-    
+
     // Extract only token and name
     const token = searchParams.get('token');
     const name = searchParams.get('name');
-    
+
     // Create new query string with only these parameters
     const newSearchParams = new URLSearchParams();
     if (token && token.trim() !== '') {
@@ -19,7 +19,7 @@ function filterQueryParams(url) {
     if (name && name.trim() !== '') {
         newSearchParams.set('name', name);
     }
-    
+
     // Update the request URL (keep pathname, replace query)
     const path = url.split('?')[0];
     return path + (newSearchParams.toString() ? `?${newSearchParams.toString()}` : '');
@@ -27,4 +27,4 @@ function filterQueryParams(url) {
 
 module.exports = {
     filterQueryParams
-}; 
+};
