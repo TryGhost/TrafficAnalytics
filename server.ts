5ac57e3cd790e7f8e97dcff8d1ecd401b20bd578
Steve Larson <9larsons@gmail.com>
2025-04-17 17:47:10 -0500

    Fixed docker build for production
    no ref

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import app from './src/app';
 const port: number = parseInt(process.env.PORT || '3000', 10);
 
 // Start the server if this file is run directly
-if (process.env.NODE_ENV !== 'production' && require.main === module) {
+if (require.main === module) {
     const start = async (): Promise<void> => {
         try {
             await app.listen({host: '0.0.0.0', port});
