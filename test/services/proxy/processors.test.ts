b3524f4d9581e328f50895ad35b2a47042c7fe43
Steve Larson <9larsons@gmail.com>
2025-04-29 10:48:13 -0500

    Updated test path

--- test/services/proxy/processors.test.ts
+++ test/services/proxy/processors.test.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect} from 'vitest';
-import * as processors from '../../../src/services/proxy/processors';
+import * as processors from '../../../src/services/proxy/processors/user-agent';
 import {FastifyRequest, HttpProxyRequest} from '../../../src/types';
 
 const {parseUserAgent} = processors;
