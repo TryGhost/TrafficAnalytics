37059199fbcd250d2aa1e3bdc16fcab2ef2b6674
Chris Raible <chris@ghost.org>
2025-05-20 15:32:13 -0700

    Reverted change to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     rules: {
         'eol-last': ['error', 'always'],
         'no-trailing-spaces': 'error',
-        '@typescript-eslint/no-explicit-any': ['warn'],
+        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': ['error', {
             argsIgnorePattern: '^_',
             varsIgnorePattern: '^_'
