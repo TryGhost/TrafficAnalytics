fe84cf6c44e06104ec70a37ceeb61010e718350e
Chris Raible <chris@ghost.org>
2025-06-03 16:30:16 -0700

    Allowed explicit any in test files

--- test/.eslintrc.cjs
+++ test/.eslintrc.cjs
@@ -2,5 +2,9 @@ module.exports = {
     plugins: ['ghost'],
     extends: [
         'plugin:ghost/ts-test'
-    ]
+    ],
+    rules: {
+        // Using explicit any in tests is fine
+        '@typescript-eslint/no-explicit-any': 'off'
+    }
 };
