99380ba114dc793c5cd30ff7f64e18648e538630
Chris Raible <chris@ghost.org>
2025-06-17 15:58:14 -0700

    Extracted logging hooks into a fastify plugin (#158)
    We've been adding lots of functionality to the main `app.ts` file and
    it's beginning to get a little hard to follow. In Fastify, it's a common
    pattern to extract functionality into plugins, and register them in the
    main app. This commit is the first move in this direction, pulling out
    the logging hooks specifically into their own plugin.

---
+++ src/plugins/logging.ts
@@ -0,0 +1,50 @@
+import {FastifyInstance} from 'fastify';
+import fp from 'fastify-plugin';
+
+async function loggingPlugin(fastify: FastifyInstance) {
+    fastify.addHook('onRequest', async (request) => {
+        const PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT;
+        const traceHeader = request.headers['x-cloud-trace-context'] as string;
+        if (traceHeader && PROJECT_ID) {
+            const traceId = traceHeader.split('/')[0];
+            const traceContext = {
+                'logging.googleapis.com/trace': `projects/${PROJECT_ID}/traces/${traceId}`
+            };
+
+            request.log = request.log.child(traceContext);
+        }
+
+        request.log.info({
+            httpRequest: {
+                requestMethod: request.method,
+                requestUrl: request.url,
+                userAgent: request.headers['user-agent'],
+                remoteIp: request.ip,
+                referer: request.headers.referer,
+                protocol: `${request.protocol.toUpperCase()}/${request.raw.httpVersion}`,
+                requestSize: String(request.raw.headers['content-length'] || 0)
+            }
+        }, 'incoming request');
+    });
+
+    fastify.addHook('onResponse', async (request, reply) => {
+        request.log.info({
+            httpRequest: {
+                requestMethod: request.method,
+                requestUrl: request.url,
+                userAgent: request.headers['user-agent'],
+                remoteIp: request.ip,
+                referer: request.headers.referer,
+                protocol: `${request.protocol.toUpperCase()}/${request.raw.httpVersion}`,
+                requestSize: String(request.raw.headers['content-length'] || 0),
+                responseSize: String(reply.getHeader('content-length') || 0),
+                status: reply.statusCode,
+                latency: `${(reply.elapsedTime / 1000).toFixed(9)}s`
+            }
+        }, 'request completed');
+    });
+}
+
+// Wrapping in `fp` makes these hooks global, so they apply to all routes
+// Without this, these hooks would only apply to routes registered in the same plugin
+export default fp(loggingPlugin);
