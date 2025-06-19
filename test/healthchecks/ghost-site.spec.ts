717f412e8372c028eea9a00b3fddd7a8f07aadb4
Chris Raible <chris@ghost.org>
2025-06-19 14:04:41 -0700

    Updated test to visit full baseURL including subdirectory

--- test/healthchecks/ghost-site.spec.ts
+++ test/healthchecks/ghost-site.spec.ts
@@ -1,12 +1,12 @@
 import {test, expect} from './fixtures';
 
 test.describe('Ghost site healthcheck', () => {
-    test('should make analytics request with 202 response', async ({page}) => {
+    test('should make analytics request with 202 response', async ({page, baseURL}) => {
         // Wait for the analytics request
         const analyticsRequestPromise = page.waitForRequest('**/.ghost/analytics/tb/web_analytics**');
         const analyticsResponsePromise = page.waitForResponse('**/.ghost/analytics/tb/web_analytics**');
         
-        await page.goto('/');
+        await page.goto(baseURL!);
         
         // Verify the analytics request was made
         const request = await analyticsRequestPromise;
