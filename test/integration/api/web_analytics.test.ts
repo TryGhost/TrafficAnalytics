c8c726ce2a6eca060f3f9cd158688c304919157e
Chris Raible <chris@ghost.org>
2025-07-02 15:36:38 -0700

    Added endpoint validation tests for the current web_analytics endpoint

--- test/integration/api/web_analytics.test.ts
+++ test/integration/api/web_analytics.test.ts
@@ -1,9 +1,50 @@
-import {describe, it} from 'vitest';
+import {describe, expect, it, beforeEach} from 'vitest';
+import createApp from '../../../src/app';
+import {FastifyInstance} from 'fastify';
+import defaultValidRequestHeaders from '../../utils/fixtures/defaultValidRequestHeaders.json';
+import defaultValidRequestBody from '../../utils/fixtures/defaultValidRequestBody.json';
+import defaultValidRequestQuery from '../../utils/fixtures/defaultValidRequestQuery.json';
+import headersWithoutSiteUuid from '../../utils/fixtures/headersWithoutSiteUuid.json';
 
-describe('Event Ingestion Endpoint', function () {
-    describe('POST /tb/web_analytics', function () {
-        it('should accept a default request from the tracking script', async function () {
+function handlerStub(_request, reply, done) {
+    reply.code(202);
+    done();
+}
 
+describe('Unversioned API Endpoint', function () {
+    let app: FastifyInstance;
+
+    describe('POST /tb/web_analytics', () => {
+        beforeEach(async function () {
+            app = createApp();
+            app.addHook('preHandler', handlerStub);
         });
+
+        describe('Request validation', function () {
+            it('should accept a default valid request from the tracking script', async function () {
+                const response = await app.inject({
+                    method: 'POST',
+                    url: '/tb/web_analytics',
+                    query: defaultValidRequestQuery,
+                    headers: defaultValidRequestHeaders,
+                    body: defaultValidRequestBody
+                });
+                expect(response.statusCode).toBe(202);
+            });
+
+            it('should reject a request with a missing site uuid header', async function () {
+                const response = await app.inject({
+                    method: 'POST',
+                    url: '/tb/web_analytics',
+                    query: defaultValidRequestQuery,
+                    headers: headersWithoutSiteUuid,
+                    body: defaultValidRequestBody
+                });
+                const bodyJson = JSON.parse(response.body);
+                expect(response.statusCode).toBe(400);
+                expect(bodyJson.error).toBe('Bad Request');
+                expect(bodyJson.message).toBe('headers must have required property \'x-site-uuid\'');
+            });
+        });    
     });
 });
