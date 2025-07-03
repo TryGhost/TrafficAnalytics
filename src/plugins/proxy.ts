4dd1bc3798e9daa9b233b8cb130d6aabda48e576
Chris Raible <chris@ghost.org>
2025-07-03 15:56:43 -0700

    Moved pageHitRouteOptions to page-hit-handlers file

--- src/plugins/proxy.ts
+++ src/plugins/proxy.ts
@@ -1,9 +1,9 @@
 import {FastifyInstance} from 'fastify';
 import fp from 'fastify-plugin';
 import {publishEvent} from '../services/events/publisher.js';
-import {PageHitRequestType, PageHitRequestQueryParamsSchema, PageHitRequestHeadersSchema, PageHitRequestBodySchema, populateAndTransformPageHitRequest} from '../schemas';
+import {PageHitRequestType} from '../schemas';
 import {pageHitRawPayloadFromRequest} from '../transformations/page-hit-transformations.js';
-import {pageHitRequestHandler} from '../handlers/page-hit-handlers.js';
+import {pageHitRouteOptions} from '../handlers/page-hit-handlers.js';
 
 export const publishPageHitRaw = async (request: PageHitRequestType): Promise<void> => {
     const topic = process.env.PUBSUB_TOPIC_PAGE_HITS_RAW as string;
@@ -16,16 +16,6 @@ export const publishPageHitRaw = async (request: PageHitRequestType): Promise<vo
             logger: request.log
         });
     }    
-};
-
-const pageHitRouteOptions = {
-    schema: {
-        querystring: PageHitRequestQueryParamsSchema,
-        headers: PageHitRequestHeadersSchema,
-        body: PageHitRequestBodySchema
-    },
-    preHandler: populateAndTransformPageHitRequest,
-    handler: pageHitRequestHandler
 };
 
 async function proxyPlugin(fastify: FastifyInstance) {
