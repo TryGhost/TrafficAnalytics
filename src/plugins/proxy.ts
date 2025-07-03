c23e9393c5de53a73c89ea62cea75b4fad823945
Chris Raible <chris@ghost.org>
2025-07-03 15:59:54 -0700

    Moved publishPageHitRaw into publisherUtils file

--- src/plugins/proxy.ts
+++ src/plugins/proxy.ts
@@ -1,22 +1,6 @@
 import {FastifyInstance} from 'fastify';
 import fp from 'fastify-plugin';
-import {publishEvent} from '../services/events/publisher.js';
-import {PageHitRequestType} from '../schemas';
-import {pageHitRawPayloadFromRequest} from '../transformations/page-hit-transformations.js';
 import {pageHitRouteOptions} from '../handlers/page-hit-handlers.js';
-
-export const publishPageHitRaw = async (request: PageHitRequestType): Promise<void> => {
-    const topic = process.env.PUBSUB_TOPIC_PAGE_HITS_RAW as string;
-    if (topic) {
-        const payload = pageHitRawPayloadFromRequest(request);
-        request.log.info({payload}, 'Publishing page hit raw event');
-        await publishEvent({
-            topic,
-            payload,
-            logger: request.log
-        });
-    }    
-};
 
 async function proxyPlugin(fastify: FastifyInstance) {
     fastify.post('/tb/web_analytics', pageHitRouteOptions);
