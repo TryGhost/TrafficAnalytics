c30ff71dad587d868d158c99f3f2b2040356af3b
Chris Raible <chris@ghost.org>
2025-07-03 12:27:23 -0700

    OLTP exporter for jaeger working

--- src/utils/instrumentation.ts
+++ src/utils/instrumentation.ts
@@ -1,25 +1,27 @@
 /*instrumentation.ts*/
 import {NodeSDK} from '@opentelemetry/sdk-node';
-import {ConsoleSpanExporter} from '@opentelemetry/sdk-trace-node';
-import {
-    PeriodicExportingMetricReader,
-    ConsoleMetricExporter
-} from '@opentelemetry/sdk-metrics';
-import {
-    ATTR_SERVICE_NAME,
-    ATTR_SERVICE_VERSION
-} from '@opentelemetry/semantic-conventions';
+import {PeriodicExportingMetricReader} from '@opentelemetry/sdk-metrics';
+import {ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION} from '@opentelemetry/semantic-conventions';
 import {resourceFromAttributes} from '@opentelemetry/resources';
+import {OTLPTraceExporter} from '@opentelemetry/exporter-trace-otlp-http';
+import {OTLPMetricExporter} from '@opentelemetry/exporter-metrics-otlp-http';
+import {getNodeAutoInstrumentations} from '@opentelemetry/auto-instrumentations-node';
 
 const sdk = new NodeSDK({
     resource: resourceFromAttributes({
         [ATTR_SERVICE_NAME]: 'analytics-service',
         [ATTR_SERVICE_VERSION]: '1.0'
     }), 
-    traceExporter: new ConsoleSpanExporter(),
+    traceExporter: new OTLPTraceExporter({
+        url: 'http://jaeger:4318/v1/traces',
+        concurrencyLimit: 100
+    }),
     metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter()
-    })
+        exporter: new OTLPMetricExporter({
+            url: 'http://jaeger:4318/v1/metrics'
+        })
+    }),
+    instrumentations: [getNodeAutoInstrumentations()]
 });
 
 sdk.start();
