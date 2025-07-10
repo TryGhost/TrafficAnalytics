b9f950abc080ddee476b3e84fb35caa66bac77e3
Chris Raible <chris@ghost.org>
2025-07-09 18:54:54 -0700

    Updated healthchecks to be compatible with new versioned endpoint

--- test/healthchecks/ghost-site.spec.ts
+++ test/healthchecks/ghost-site.spec.ts
@@ -3,14 +3,14 @@ import {test, expect} from './fixtures';
 test.describe('Ghost site healthcheck', () => {
     test('should make analytics request with 202 response', async ({page, baseURL}) => {
         // Wait for the analytics request
-        const analyticsRequestPromise = page.waitForRequest('**/.ghost/analytics/tb/web_analytics**');
-        const analyticsResponsePromise = page.waitForResponse('**/.ghost/analytics/tb/web_analytics**');
+        const analyticsRequestPromise = page.waitForRequest('**/.ghost/analytics/**');
+        const analyticsResponsePromise = page.waitForResponse('**/.ghost/analytics/**');
         
         await page.goto(baseURL!);
         
         // Verify the analytics request was made
         const request = await analyticsRequestPromise;
-        expect(request.url()).toContain('/.ghost/analytics/tb/web_analytics');
+        expect(request.url()).toContain('/.ghost/analytics/');
         
         // Verify the response status is 202
         const response = await analyticsResponsePromise;
