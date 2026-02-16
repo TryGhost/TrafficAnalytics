16a00bf9a35bd8a775d3aad8c25b6c1075dfd740
Chris Raible <chris@ghost.org>
2026-02-16 15:02:48 -0800

    Extracted logger-config from the logger itself

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
-export {default as logger, getLoggerConfig} from './logger';
+export {default as logger} from './logger';
+export {getLoggerConfig} from './logger-config';
 export * from './query-params';
